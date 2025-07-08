1f65d35de5ed49f6b5f92c6c6aaf70f164586d18
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-07-08 14:22:44 +0900

    feat: x-idが無い場合でもリクエストを処理できるように

--- src/server/api/object/write.rs
+++ src/server/api/object/write.rs
@@ -20,23 +20,72 @@ pub struct ReqParams {
     part_number: Option<u32>,
 }
 
-pub async fn write_handler(Path(object_path): Path<String>, Query(params): Query<ReqParams>) -> AppResult<impl IntoResponse> {
+#[derive(PartialEq)]
+enum OperationType {
+    PutObject,
+    CreateMultipartUpload,
+    UploadPart,
+    CompleteMultipartUpload,
+    AbortMultipartUpload,
+    DeleteObject,
+    Unknown,
+}
+
+pub async fn write_handler(Path(object_path): Path<String>, Query(params): Query<ReqParams>, request: Request<Body>) -> AppResult<impl IntoResponse> {
     if object_path.is_empty() {
         return Ok(StatusCode::BAD_REQUEST.into_response());
     }
 
-    if params.x_id.is_none() {
-        return Ok((StatusCode::BAD_REQUEST, "x-id is required").into_response());
+    let operation = match params.x_id.unwrap_or_default().as_str() {
+        "PutObject" => OperationType::PutObject,
+        "CreateMultipartUpload" => OperationType::CreateMultipartUpload,
+        "UploadPart" => OperationType::UploadPart,
+        "CompleteMultipartUpload" => OperationType::CompleteMultipartUpload,
+        "AbortMultipartUpload" => OperationType::AbortMultipartUpload,
+        "DeleteObject" => OperationType::DeleteObject,
+        _ => {
+            match *request.method() {
+                Method::PUT => {
+                    if params.upload_id.is_some() {
+                        OperationType::UploadPart
+                    } else {
+                        OperationType::PutObject
+                    }
+                }
+                Method::POST => {
+                    if params.upload_id.is_some() {
+                        OperationType::CompleteMultipartUpload
+                    } else {
+                        OperationType::CreateMultipartUpload
+                    }
+                }
+                Method::DELETE => {
+                    if params.upload_id.is_some() {
+                        OperationType::AbortMultipartUpload
+                    } else {
+                        OperationType::DeleteObject
+                    }
+                },
+                _ => OperationType::Unknown,
+            }
+        },
+    };
+
+    if operation == OperationType::Unknown {
+        return Ok((StatusCode::BAD_REQUEST, "unknown operation").into_response());
     }
 
-    match params.x_id.unwrap().as_str() {
-        "PutObject" => {
+    let mime_type = get_header(&request, "Content-Type", Some("application/octet-stream".to_string()));
+    let content_size = get_header(&request, "Content-Length", None).parse::<i64>().unwrap_or(0);
+    let filename = parse_content_disposition(get_header(&request, "Content-Disposition", None).as_str()).unwrap_or("unknown".to_string());
+    let (_, body) = request.into_parts();
+
+    match operation {
+        OperationType::PutObject => {
             // TODO
         }
         _ => {
             return Ok((StatusCode::BAD_REQUEST, "Unknown operation type").into_response());
         }
     }
-
-    return Ok((StatusCode::NOT_IMPLEMENTED, "write_handler is not implemented yet").into_response());
 }
