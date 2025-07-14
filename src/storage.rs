12cea77a4214d63ffaeb0545e941f789e841a3af
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-07-14 13:57:29 +0900

    feat: MultipartUpload経由のオブジェクトの読み書きに対応

--- src/storage.rs
+++ src/storage.rs
@@ -1,10 +1,22 @@
-use std::fs;
-use std::path::Path;
-use std::process;
+use std::{
+    collections::HashMap,
+    fs,
+    future::Future,
+    path::Path,
+    pin::Pin,
+    process,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc, LazyLock, Mutex,
+    },
+    time::Duration,
+};
 use anyhow::Error;
 use axum::body::BodyDataStream;
+use chrono::{DateTime, TimeDelta, Utc};
 use sea_orm::ActiveValue::Set;
-use tokio::fs::File;
+use tokio::{fs::File, time};
+use uuid::Uuid;
 use crate::entity;
 use crate::config;
 
@@ -22,8 +34,24 @@ pub struct WriteObjectData {
     pub binary: BodyDataStream,
     pub path: String,
     pub filename: String,
+    pub encoded_filename: Option<String>,
     pub content_size: i64,
     pub mime_type: String,
+}
+
+// Multipart upload state management
+pub type MultipartUploadState = Arc<Mutex<HashMap<String, MultipartUploadItem>>>;
+pub static MULTIPART_UPLOAD_STATE: LazyLock<MultipartUploadState> = LazyLock::new(|| {
+    Arc::new(Mutex::new(HashMap::new()))
+});
+
+#[derive(Clone)]
+pub struct MultipartUploadItem {
+    pub path: String,
+    pub filename: String,
+    pub encoded_filename: Option<String>,
+    pub mime_type: String,
+    pub last_upload_at: DateTime<Utc>,
 }
 
 pub async fn initialize() {
@@ -35,6 +63,13 @@ pub async fn initialize() {
             process::exit(1);
         } else {
             tracing::info!("Bucket dir created successfully: {}", base_path.display());
+        }
+    }
+
+    let temp_path = base_path.join(".multipart");
+    if temp_path.exists() {
+        if let Err(e) = fs::remove_dir_all(&temp_path) {
+            tracing::error!("Failed to remove expired multipart uploads: {}", e);
         }
     }
 }
@@ -54,19 +89,156 @@ pub async fn get_object(path: String, with_file: bool) -> Result<ReadObjectData,
     })
 }
 
-pub async fn write_object(data: WriteObjectData) -> Result<(), Error> {
+pub async fn put_object(data: WriteObjectData) -> Result<(), Error> {
     let internal_path = blake3::hash(data.path.as_bytes()).to_hex().to_string();
     let metadata = entity::object::ActiveModel {
         internal_filename: Set(internal_path.clone()),
         path: Set(data.path),
         filename: Set(data.filename),
+        encoded_filename: Set(data.encoded_filename),
         content_size: Set(data.content_size),
         mime_type: Set(data.mime_type),
         ..Default::default()
     };
 
     metadata::create_metadata(metadata).await?;
-    file::write_object(internal_path.clone(), data.binary).await?;
+    file::write_object(internal_path.clone(), data.binary, false).await?;
 
     return Ok(());
+}
+
+pub fn create_multipart_upload(path: String, filename: String, encoded_filename: Option<String>, mime_type: String) -> String {
+    let upload_id = Uuid::new_v4().to_string();
+    let item = MultipartUploadItem {
+        path,
+        filename,
+        encoded_filename,
+        mime_type,
+        last_upload_at: Utc::now(),
+    };
+
+    MULTIPART_UPLOAD_STATE.lock().unwrap().insert(upload_id.clone(), item);
+    tracing::debug!("Multipart upload created with ID: {}", upload_id);
+
+    tokio::spawn(internal_cleanup());
+    return upload_id;
+}
+
+pub async fn upload_part(upload_id: String, number: u16, binary: BodyDataStream) -> Result<(), Error> {
+    {
+        let mut upload_item = MULTIPART_UPLOAD_STATE.lock().unwrap();
+        let item = upload_item.get_mut(&upload_id);
+        if item.is_none() {
+            return Err(anyhow::anyhow!("Invalid or expired uploadId: {}", upload_id));
+        }
+        item.unwrap().last_upload_at = Utc::now();
+    }
+
+    file::write_object(format!("{}/{}.part", upload_id, number), binary, true).await?;
+    return Ok(());
+}
+
+pub async fn complete_multipart_upload(upload_id: String) -> Result<(), Error> {
+    let upload_item = {
+        let mut state = MULTIPART_UPLOAD_STATE.lock().unwrap();
+        state.remove(&upload_id)
+    };
+
+    if upload_item.is_none() {
+        return Err(anyhow::anyhow!("Invalid or expired uploadId: {}", upload_id));
+    }
+
+    let item = upload_item.unwrap();
+    let internal_filename = blake3::hash(item.path.as_bytes()).to_hex().to_string();
+    let file_size = file::merge_partial_uploads(&upload_id, &internal_filename.clone()).await?;
+    let metadata = entity::object::ActiveModel {
+        internal_filename: Set(internal_filename),
+        path: Set(item.path),
+        filename: Set(item.filename),
+        encoded_filename: Set(item.encoded_filename),
+        content_size: Set(file_size as i64),
+        mime_type: Set(item.mime_type),
+        ..Default::default()
+    };
+
+    metadata::create_metadata(metadata).await?;
+    file::delete_object(upload_id.clone(), true).await?;
+
+    tracing::debug!("Multipart upload completed for ID: {}", upload_id);
+    return Ok(());
+}
+
+pub async fn abort_multipart_upload(upload_id: String) -> Result<(), Error> {
+    {
+        let mut state = MULTIPART_UPLOAD_STATE.lock().unwrap();
+        state.remove(&upload_id);
+    }
+
+    if let Err(e) = file::delete_object(upload_id.clone(), true).await {
+        tracing::error!("Failed to remove multipart upload directory: {}", e);
+        return Err(e.into());
+    }
+
+    tracing::debug!("Multipart upload aborted for ID: {}", upload_id);
+    return Ok(());
+}
+
+static IS_CLEANUP_REGISTERED: AtomicBool = AtomicBool::new(false);
+
+fn internal_cleanup() -> Pin<Box<dyn Future<Output=Result<(), ()>> + Send>> {
+    Box::pin(async move {
+        if IS_CLEANUP_REGISTERED.load(Ordering::SeqCst) {
+            tracing::debug!("Cleanup already registered, skipping...");
+            return Ok(());
+        }
+
+        let most_recent_item = {
+            let state = MULTIPART_UPLOAD_STATE.lock().unwrap();
+            let most_recent_item = state.values().min_by_key(|item| item.last_upload_at);
+
+            if most_recent_item.is_none() {
+                tracing::debug!("No cleanup needed, skipping...");
+                return Ok(());
+            }
+
+            most_recent_item.unwrap().clone()
+        };
+
+        let exec_sec = config::CONFIG.bucket.request_expiration_seconds - (Utc::now() - most_recent_item.last_upload_at).num_seconds();
+        if exec_sec > 0 {
+            tracing::debug!("Scheduling cleanup in {} seconds...", exec_sec);
+            IS_CLEANUP_REGISTERED.store(true, Ordering::SeqCst);
+            time::sleep(Duration::from_secs((exec_sec as u64) + 1)).await;
+        }
+
+        tracing::debug!("Starting cleanup of expired multipart uploads...");
+        let expired_uploads = {
+            let state = MULTIPART_UPLOAD_STATE.lock().unwrap();
+            let now = Utc::now();
+            let diff = TimeDelta::seconds(config::CONFIG.bucket.request_expiration_seconds);
+            state.iter()
+                .filter_map(|(id, item)| {
+                    if now - item.last_upload_at > diff {
+                        Some(id.clone())
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<String>>()
+        };
+
+        for upload_id in expired_uploads {
+            tracing::debug!("Removing expired multipart upload: {}", upload_id);
+            if let Err(e) = file::delete_object(upload_id.clone(), true).await {
+                tracing::error!("Failed to remove expired multipart upload {}: {}", upload_id, e);
+            }
+            MULTIPART_UPLOAD_STATE.lock().unwrap().remove(&upload_id);
+        }
+
+        IS_CLEANUP_REGISTERED.store(false, Ordering::SeqCst);
+        tracing::debug!("Cleanup completed.");
+
+        tokio::spawn(internal_cleanup());
+        Ok(())
+    })
 }
--- src/storage/file.rs
+++ src/storage/file.rs
@@ -1,13 +1,13 @@
 use std::path::{Path, PathBuf};
 use anyhow::Error;
 use axum::body::BodyDataStream;
-use tokio::{fs::{self, File}, io::{AsyncWriteExt, BufWriter}};
+use tokio::{fs::{self, File, OpenOptions}, io::{self, AsyncWriteExt, BufWriter}};
 use tokio_stream::StreamExt;
 
 use crate::config;
 
 pub async fn read_object(internal_filename: String) -> Result<File, Error> {
-    let path = resolve_path(internal_filename);
+    let path = resolve_path(internal_filename, false);
     if !path.exists() {
         return Err(anyhow::anyhow!("File does not exist"));
     }
@@ -17,16 +17,17 @@ pub async fn read_object(internal_filename: String) -> Result<File, Error> {
         return Err(anyhow::anyhow!("Failed to open file: {}", file.as_ref().err().unwrap()));
     }
 
+    tracing::debug!("Object read successfully from path: {}", path.display());
     return Ok(file.unwrap());
 }
 
-pub async fn write_object(internal_filename: String, mut stream: BodyDataStream, create_dir: bool) -> Result<(), Error> {
-    let path = resolve_path(internal_filename);
+pub async fn write_object(internal_filename: String, mut stream: BodyDataStream, is_multipart: bool) -> Result<(), Error> {
+    let path = resolve_path(internal_filename, is_multipart);
     if path.exists() {
         return Err(anyhow::anyhow!("File already exists at path: {}", path.display()));
     }
 
-    if create_dir && let Some(parent) = path.parent() {
+    if is_multipart && let Some(parent) = path.parent() {
         if !parent.exists() {
             fs::create_dir_all(parent).await?;
         }
@@ -38,9 +39,84 @@ pub async fn write_object(internal_filename: String, mut stream: BodyDataStream,
     }
 
     writer.flush().await?;
+
+    tracing::debug!("Object written successfully to path: {}", path.display());
     return Ok(());
 }
 
-fn resolve_path(internal_filename: String) -> PathBuf {
-    return Path::new(&config::CONFIG.bucket.path).join(internal_filename);
+pub async fn merge_partial_uploads(upload_id: &String, internal_filename: &String) -> Result<u64, Error> {
+    let object_path = resolve_path(internal_filename.clone(), false);
+    let multipart_path = resolve_path(upload_id.clone(), true);
+    let temporary_output_path = multipart_path.join("object-merged.tmp");
+
+    if !multipart_path.exists() {
+        return Err(anyhow::anyhow!("Multipart upload path does not exist: {}", multipart_path.display()));
+    }
+
+    if object_path.exists() {
+        return Err(anyhow::anyhow!("File already exists at path: {}", object_path.display()));
+    }
+
+    let mut multipart_files = fs::read_dir(&multipart_path).await?;
+    let mut file_list = Vec::new();
+
+    while let Some(entry) = multipart_files.next_entry().await? {
+        if entry.file_type().await?.is_file() {
+            file_list.push(entry.path());
+        }
+    };
+
+    file_list.sort_by(|a, b| {
+        let a_name = a.file_name().unwrap().to_str().unwrap().replace(".part", "");
+        let b_name = b.file_name().unwrap().to_str().unwrap().replace(".part", "");
+        let a_num = a_name.parse::<u64>().unwrap_or(0);
+        let b_num = b_name.parse::<u64>().unwrap_or(0);
+        a_num.cmp(&b_num)
+    });
+
+    let output_file = OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(&temporary_output_path)
+        .await?;
+
+    let mut writer = BufWriter::new(output_file);
+
+    for file in file_list {
+        let mut input_file = File::open(file).await?;
+        io::copy(&mut input_file, &mut writer).await?;
+    }
+
+    writer.flush().await?;
+    fs::rename(&temporary_output_path, &object_path).await?;
+
+    tracing::debug!("Merged multipart uploads into object at path: {}", object_path.display());
+    return Ok(fs::metadata(&object_path).await?.len());
+}
+
+pub async fn delete_object(internal_path: String, is_multipart: bool) -> Result<(), Error> {
+    let path = resolve_path(internal_path, is_multipart);
+
+    if is_multipart {
+        fs::remove_dir_all(&path).await?;
+    } else {
+        if !path.exists() {
+            return Err(anyhow::anyhow!("File does not exist at path: {}", path.display()));
+        }
+
+        fs::remove_file(&path).await?;
+    }
+
+    tracing::debug!("Deleted object at path: {}", path.display());
+    return Ok(());
+}
+
+fn resolve_path(internal_path: String, is_multipart: bool) -> PathBuf {
+    let base = Path::new(&config::CONFIG.bucket.path);
+    if is_multipart {
+        base.join(".multipart").join(internal_path)
+    } else {
+        base.join(internal_path)
+    }
 }
