898a902624442c662a67477963745cb1acde1b86
yukineko <27853966+hideki0403@users.noreply.github.com>
2025-07-14 13:51:45 +0900

    fix: objectへのgetリクエストで正しくheaderがセットされていない問題を修正

--- src/server/api/object/read.rs
+++ src/server/api/object/read.rs
@@ -1,10 +1,11 @@
-use axum::{extract::Path, http::{HeaderMap, Method, StatusCode}, response::IntoResponse};
+use axum::{body::Body, extract::Request, http::{HeaderMap, Method, StatusCode}, response::IntoResponse};
 use axum_range::{Ranged, KnownSize};
 use axum_extra::{headers::Range, TypedHeader};
-use crate::server::{AppError, AppResult};
+use crate::server::{AppResult, utils::build_content_disposition_filename};
 use crate::storage;
 
-pub async fn read_handler(method: Method, Path(object_path): Path<String>, range: Option<TypedHeader<Range>>) -> AppResult<impl IntoResponse> {
+pub async fn read_handler(method: Method, range: Option<TypedHeader<Range>>, request: Request<Body>) -> AppResult<impl IntoResponse> {
+    let object_path = request.uri().path().to_string();
     if object_path.is_empty() {
         return Ok(StatusCode::BAD_REQUEST.into_response());
     }
@@ -12,7 +13,7 @@ pub async fn read_handler(method: Method, Path(object_path): Path<String>, range
     let is_head_request = method == Method::HEAD;
     let object_data = storage::get_object(object_path, !is_head_request).await;
     if object_data.is_err() {
-        return Err(AppError(object_data.unwrap_err()));
+        return Ok((StatusCode::NOT_FOUND, "Object not found").into_response());
     }
     let object_data = object_data.unwrap();
 
@@ -20,14 +21,18 @@ pub async fn read_handler(method: Method, Path(object_path): Path<String>, range
     let mut headers = HeaderMap::new();
     headers.insert("Cache-Control", "max-age=31536000, immutable".parse().unwrap());
     headers.insert("Content-Type", object_data.metadata.mime_type.parse().unwrap());
-    headers.insert("Content-Length", object_data.metadata.content_size.to_string().parse().unwrap());
-    headers.insert("Content-Disposition", format!("inline; filename=\"{}\"", object_data.metadata.filename).parse().unwrap());
+    headers.insert("Content-Disposition", format!("inline; {}", build_content_disposition_filename(object_data.metadata.filename, object_data.metadata.encoded_filename)).parse().unwrap());
     headers.insert("ETag", format!("\"{}\"", object_data.metadata.internal_filename).parse().unwrap());
+    headers.insert("Accept-Ranges", "bytes".parse().unwrap());
 
     if is_head_request {
+        headers.insert("Content-Length", object_data.metadata.content_size.to_string().parse().unwrap());
         return Ok((StatusCode::OK, headers).into_response());
     }
 
     let range = range.map(|TypedHeader(range)| range);
-    return Ok(Ranged::new(range, KnownSize::file(object_data.file.unwrap()).await.unwrap()).into_response());
+    let mut response = Ranged::new(range, KnownSize::file(object_data.file.unwrap()).await.unwrap()).into_response();
+    response.headers_mut().extend(headers);
+
+    return Ok(response);
 }
